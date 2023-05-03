f1922bd7e9fbe0bfedcd6df6cc29540c9a2bd09f
Sebouh Paul <sebouh.paul@gmail.com>
2023-05-02 23:24:34 -0400

    corrected parameters for the beampipe

--- beampipe_parameters.py
+++ beampipe_parameters.py
@@ -7,9 +7,8 @@ class Beampipe:
     def holeR(self, layer):
         z=hcal_start_z+layer*2.34
         clearance=3.85
-	beampipe_thickness=0.2
-        return clearance + beampipe_thickness 
-+ 4.403+(z-122.864)*(14.592-4.403)/(522.664-122.864)
+        beampipe_thickness=0.2
+        return clearance + beampipe_thickness + 4.403+(z-122.864)*(14.592-4.403)/(522.664-122.864)
     def beampipeX(self, layer):
         z=hcal_start_z+layer*2.34
         return -1.320+(z-122.864)*(-11.252+1.329)/(522.664-122.864)
